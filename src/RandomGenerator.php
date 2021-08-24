a849fdcf8e072030f197f2406f0c1c47034bed5a
fashxp <fashxp@users.noreply.github.com>
2021-08-24 10:39:05 +0000

    Apply php-cs-fixer changes

--- src/RandomGenerator.php
+++ src/RandomGenerator.php
@@ -9,8 +9,8 @@
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PEL
  */
 
 namespace Pimcore\Bundle\NumberSequenceGeneratorBundle;
@@ -20,7 +20,6 @@ use Symfony\Component\Lock\Factory as LockFactory;
 
 class RandomGenerator
 {
-
     /**
      * @var LockFactory
      */
@@ -29,43 +28,41 @@ class RandomGenerator
     /**
      *  key for lock table
      */
-
-    const LOCK_KEY = "number_sequence_generator";
+    const LOCK_KEY = 'number_sequence_generator';
     /**
      * numeric code
      */
-    const NUMERIC = "numeric";
+    const NUMERIC = 'numeric';
 
     /**
      * alphanumeric code
      */
-    const ALPHANUMERIC = "alphanumeric";
+    const ALPHANUMERIC = 'alphanumeric';
 
     /**
      * table name
      */
-    const TABLE_NAME = "bundle_number_sequence_generator_randomregister";
+    const TABLE_NAME = 'bundle_number_sequence_generator_randomregister';
 
     public function __construct(LockFactory $lockFactory)
     {
         $this->lockFactory = $lockFactory;
     }
 
-
     /**
      * @param $range
      * @param string $codeType
      * @param null $length
      * @param string $characterSet character set for alphanumeric codes
+     *
      * @return bool|int|string
      */
     public function generateCode(
         $range,
         $codeType = self::NUMERIC,
         $length = null,
-        $characterSet = "123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ"
+        $characterSet = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ'
     ) {
-
         switch ($codeType) {
             case self::NUMERIC:
                 return $this->generateNumericCode($range);
@@ -78,6 +75,7 @@ class RandomGenerator
 
     /**
      * @param $range
+     *
      * @return int
      */
     private function generateNumericCode($range)
@@ -85,15 +83,14 @@ class RandomGenerator
         $lock = $this->lockFactory->createLock(self::LOCK_KEY);
         $lock->acquire(true);
         $db = Db::get();
-        $code = $db->fetchOne("SELECT `code` FROM ".self::TABLE_NAME." WHERE `range` = ?", [$range]);
+        $code = $db->fetchOne('SELECT `code` FROM '.self::TABLE_NAME.' WHERE `range` = ?', [$range]);
 
         if ($code) {
             $code++;
-            $db->update(self::TABLE_NAME, ["code" => $code], "`range` = ".$db->quote($range));
-
+            $db->update(self::TABLE_NAME, ['code' => $code], '`range` = '.$db->quote($range));
         } else {
             $code = 1;
-            $db->insert(self::TABLE_NAME, ["code" => $code, "range" => $range]);
+            $db->insert(self::TABLE_NAME, ['code' => $code, 'range' => $range]);
         }
         $lock->release();
 
@@ -103,12 +100,13 @@ class RandomGenerator
     /**
      * @param $range
      * @param $length
+     *
      * @return bool|string
      */
     private function generateAlphanumericCode($range, $length, $characterSet)
     {
         if ($length && $length > 50) {
-            throw new \Exception("maximum code length is 50");
+            throw new \Exception('maximum code length is 50');
         }
 
         $lock = $this->lockFactory->createLock(self::LOCK_KEY);
@@ -119,18 +117,17 @@ class RandomGenerator
         while ($result) {
             $code = substr(str_shuffle($characterSet), 0, $length);
             $result = $db->fetchRow(
-                "SELECT * FROM ".self::TABLE_NAME." WHERE `range` = ? AND `code` = ?",
+                'SELECT * FROM '.self::TABLE_NAME.' WHERE `range` = ? AND `code` = ?',
                 [$range, $code]
             );
         }
 
-        $db->insert(self::TABLE_NAME, ["range" => $range, "code" => $code]);
+        $db->insert(self::TABLE_NAME, ['range' => $range, 'code' => $code]);
 
         $lock->release();
 
         return $code;
     }
-
 
     /**
      * @param $range
@@ -138,7 +135,6 @@ class RandomGenerator
     public function resetCodeGenerator($range)
     {
         $db = Db::get();
-        $db->query("DELETE FROM ".self::TABLE_NAME." WHERE `range` = ?", [$range]);
+        $db->query('DELETE FROM '.self::TABLE_NAME.' WHERE `range` = ?', [$range]);
     }
-
 }

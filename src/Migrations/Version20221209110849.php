d1eb7c3bcd3c34b9223d95cf94d95de040637fe8
mattamon <mattamon@users.noreply.github.com>
2022-12-12 08:05:02 +0000

    Apply php-cs-fixer changes

--- src/Migrations/Version20221209110849.php
+++ src/Migrations/Version20221209110849.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
 namespace Pimcore\Bundle\NumberSequenceGeneratorBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -12,7 +25,6 @@ use Pimcore\Model\Tool\SettingsStore;
  */
 class Version20221209110849 extends AbstractPimcoreMigration
 {
-
     public function doesSqlMigrations(): bool
     {
         return false;
@@ -20,19 +32,15 @@ class Version20221209110849 extends AbstractPimcoreMigration
 
     public function up(Schema $schema): void
     {
-
         $result1 = \Pimcore\Db::get()->fetchAll('SHOW TABLES LIKE "bundle_number_sequence_generator_register"');
         $result2 = \Pimcore\Db::get()->fetchAll('SHOW TABLES LIKE "' . RandomGenerator::TABLE_NAME . '"');
 
         $installed = !empty($result1) && !empty($result2);
 
         SettingsStore::set('BUNDLE_INSTALLED__Pimcore\\NumberSequenceGeneratorBundle\\NumberSequenceGeneratorBundle', $installed, 'bool', 'pimcore');
-
     }
 
     public function down(Schema $schema): void
     {
     }
-
-
 }
